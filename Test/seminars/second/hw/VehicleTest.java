2cd76c37b32bc50068ae4bbf6d691bb7989e1c0d
KorolevAV <antoshka.992@mail.ru>
2023-01-23 18:51:24 +0300

    add global car and motocycle

--- Test/seminars/second/hw/VehicleTest.java
+++ Test/seminars/second/hw/VehicleTest.java
@@ -1,47 +1,46 @@
 package seminars.second.hw;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import homeworks.hw2.*;
+import org.junit.jupiter.params.ParameterizedTest;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.assertj.core.api.Assertions.*;
 
 class VehicleTest {
+    private Car car = new Car("Lada", "Vesta", 2016);
+    private Motorcycle motorcycle = new Motorcycle("Dodge", "Tomahawk", 2018);
+
     @Test
     public void testCarIsInstanceOfVehicle() {
-        Car car = new Car("Lada", "Vesta", 2016);
         assertTrue(car instanceof Vehicle);
     }
 
     @Test
     public void testCarNumWheelsEqualTo4() {
-        Car car = new Car("Lada", "Vesta", 2016);
         assertThat(car.getNumWheels()).isEqualTo(4);
     }
 
     @Test
     public void testMotoNumWheelsEqualTo4() {
-        Motorcycle motorcycle = new Motorcycle("Dodge", "Tomahawk", 2018);
         assertThat(motorcycle.getNumWheels()).isEqualTo(2);
     }
 
     @Test
     public void testCarSpeedTestDrive() {
-        Car car = new Car("Lada", "Vesta", 2016);
         car.testDrive();
         assertThat(car.getSpeed()).isEqualTo(60);
     }
 
     @Test
     public void testMotoSpeedTestDrive() {
-        Motorcycle motorcycle = new Motorcycle("Dodge", "Tomahawk", 2018);
         motorcycle.testDrive();
         assertThat(motorcycle.getSpeed()).isEqualTo(75);
     }
 
     @Test
     public void testCarSpeedPark() {
-        Car car = new Car("Lada", "Vesta", 2016);
         car.testDrive();
         car.park();
         assertThat(car.getSpeed()).isEqualTo(0);
@@ -49,7 +48,6 @@ class VehicleTest {
 
     @Test
     public void testMotoSpeedPark() {
-        Motorcycle motorcycle = new Motorcycle("Dodge", "Tomahawk", 2018);
         motorcycle.testDrive();
         motorcycle.park();
         assertThat(motorcycle.getSpeed()).isEqualTo(0);
